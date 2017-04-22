1b8ea85105ecb2f0ccf3663f7069a9b5a8fd3161
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2017-04-22 05:11:05 -0700

    Fix travis tests.

--- config/travis.exs
+++ config/travis.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
-config :ifires_sunshine_battleacademy, IfiresSunshineBattleacademy.Web.Endpoint,
+config :sunshines_battleacademy, SunshinesBattleacademy.Web.Endpoint,
   http: [port: 4001],
   server: false
 
@@ -10,8 +10,8 @@ config :ifires_sunshine_battleacademy, IfiresSunshineBattleacademy.Web.Endpoint,
 config :logger, level: :warn
 
 # Configure your database
-config :ifires_sunshine_battleacademy, IfiresSunshineBattleacademy.Repo,
+config :sunshines_battleacademy, SunshinesBattleacademy.Repo,
   adapter: Sqlite.Ecto2,
-  database: "fires_sunshine_battleacademy_test.sqlite3",
+  database: "sunshines_battleacademy_test.sqlite3",
   pool: Ecto.Adapters.SQL.Sandbox
  
