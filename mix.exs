407bc86b24262eae1a9ef05000b3016546549b23
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2017-04-23 13:04:12 -0700

    Add uuid library.

--- mix.exs
+++ mix.exs
@@ -29,6 +29,7 @@ defmodule SunshinesBattleacademy.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [{:phoenix, "~> 1.3.0-rc"},
+     {:uuid, "~> 1.1"},
      {:phoenix_pubsub, "~> 1.0"},
      {:riak, "~> 1.0"},
      {:phoenix_ecto, "~> 3.2"},
