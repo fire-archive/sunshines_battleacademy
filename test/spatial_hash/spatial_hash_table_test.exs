e531d19d6836dbf8747ab325c472e5ae679a2aba
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2017-04-23 23:34:04 -0700

    Remove spatial hash tests.

--- test/spatial_hash/spatial_hash_table_test.exs
+++ test/spatial_hash/spatial_hash_table_test.exs
@@ -1,22 +1,34 @@
 defmodule SunshinesBattleacademy.SpatialHashTable.HashTableTest do
   use SunshinesBattleacademy.SpatialCase
 
-  test "Hash", %{} do
-    assert 19349669 == SunshinesBattleacademy.SpatialHash.hash(0, 1, 0)
-  end
+  # test "Hash", %{} do
+  #   assert 19349669 == SunshinesBattleacademy.SpatialHash.hash(0, 1, 0)
+  # end
 
-  test "Insert into hash table", %{} do
-    assert :ok == SunshinesBattleacademy.SpatialHashTable.insert(%{x: 0,y: 1,z: 0}, [])
-    assert :ok == SunshinesBattleacademy.SpatialHashTable.insert(%{x: 0,y: 1,z: 0}, [])
-    assert :ok == SunshinesBattleacademy.SpatialHashTable.insert(%{x: 0,y: 1,z: 0}, [])
-  end
+  # test "Insert into hash table", %{} do
+  #   assert :ok == SunshinesBattleacademy.SpatialHashTable.insert(%{x: 0,y: 1,z: 0}, [])
+  # end
 
-  test "Find hash entry" do
-    pos = %{x: 0,y: 1,z: 0}
-    SunshinesBattleacademy.SpatialHashTable.insert(pos, %{position: pos, pointer: 0})
-    assert %{pointer: 0, position: %{x: 0, y: 1, z: 0}} = SunshinesBattleacademy.SpatialHashTable.find(%{x: 0, y: 1, z: 0})
-  end
+  # test "Find hash entry" do
+  #   pos = %{x: 0,y: 1,z: 0}
+  #   SunshinesBattleacademy.SpatialHashTable.insert(pos, %{position: pos, pointer: 0})
+  #   assert %{pointer: 0, position: %{x: 0, y: 1, z: 0}} = SunshinesBattleacademy.SpatialHashTable.find(%{x: 0, y: 1, z: 0})
+  # end
 
-  test "Test hash collision" do
-  end
+  # test "Find non-existent hash entry" do
+  #   pos = %{x: 0,y: 1,z: 0}
+  #   assert  {:error, "Can't find position"} = SunshinesBattleacademy.SpatialHashTable.find(pos)
+  # end
+
+  # test "Delete hash entry" do
+  #   pos = %{x: 0,y: 1,z: 0}
+  #   assert :ok == SunshinesBattleacademy.SpatialHashTable.insert(pos)
+  #   assert :ok == SunshinesBattleacademy.SpatialHashTable.delete(%{x: 0,y: 1,z: 0})
+  # end
+
+  # test "Test hash collision" do
+  #   #SunshinesBattleacademy.SpatialHashTable.insert(%{x: 0,y: 1,z: 0}, [])
+  #   #SunshinesBattleacademy.SpatialHashTable.insert(%{x: 0,y: 1,z: 0}, [])
+  #   #SunshinesBattleacademy.SpatialHashTable.insert(%{x: 0,y: 1,z: 0}, [])
+  # end
 end
