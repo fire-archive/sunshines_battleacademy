a20daf8d0eb69c234a2e4115233a6bf60278d27f
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2017-04-23 23:34:48 -0700

    Submit wip spatial hash table.

--- lib/sunshines_battleacademy/spatial_hash/spatial_hash_table.ex
+++ lib/sunshines_battleacademy/spatial_hash/spatial_hash_table.ex
@@ -1,21 +1,27 @@
 defmodule SunshinesBattleacademy.SpatialHashTable do
-  def insert(pos, data) do
+  # Insert position and get %{pos, block_pointer}
+  def insert(pos) do
     key = SunshinesBattleacademy.SpatialHash.hash(pos.x, pos.y, pos.z)
-    case ConCache.insert_new(:hash_table, key, %ConCache.Item{value: data, ttl: 0}) do
-            {:error, :already_exists} ->
-              ConCache.update_existing(:hash_table, key, fn(old_value) ->
-                # TODO FIX
-                {:ok, %ConCache.Item{value: data, ttl: :no_update}}
-              end)
-            other -> other
-    end
+    block_pointer = 1
+    ConCache.insert(:hash_table, key, %ConCache.Item{value: %{pos: pos, block_pointer: block_pointer}, ttl: 0})
   end
 
   def find(pos) do
     key = SunshinesBattleacademy.SpatialHash.hash(pos.x, pos.y, pos.z)
-    ConCache.get(:hash_table, key).value
+    case ConCache.get(:hash_table, key) do
+      nil -> {:error, "Can't find position"}
+      other -> other.value
+    end
   end
 
   def delete(pos) do
+    key = SunshinesBattleacademy.SpatialHash.hash(pos.x, pos.y, pos.z)
+    # linear walk
+    for n <- ConCache.get(:hash_table, key).value do
+      if n == "something" do
+      else
+        n
+      end
+    end
   end
 end
